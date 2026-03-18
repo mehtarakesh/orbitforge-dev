337f7163d8231ae68abe760aa6dcd6c0f5bae772
sourceton <159575375+sourceton@users.noreply.github.com>
2026-03-17 23:03:00 -0400

    Remove web app from public repo

--- apps/web/src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import AdminDashboard from '@/components/admin/AdminDashboard';
-
-export default function AdminPage() {
-  return <AdminDashboard />;
-}
